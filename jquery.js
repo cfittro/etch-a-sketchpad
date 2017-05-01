8d0d80d3dd7fd7ad877cb20e61cac036b592d2b6
cfittro <cdfittro@gmail.com>
2017-05-01 11:57:20 -0700

    added hover effect

--- jquery.js
+++ jquery.js
@@ -1,9 +1,15 @@
 var $boxes = 16
 
 $(document).ready(function () { 
+
     for (var i = 1; i <= $boxes; i++) {
         $(".container").append("<div class=sub></div>");}
     
+    $(".sub").hover(function() {
+        $(this).css("backgroundColor", "black");
+    }, function() {
+        $(this).css("backgroundColor", "white");
+    });
 
 
 
