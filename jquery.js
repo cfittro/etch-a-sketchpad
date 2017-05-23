af66340514c849df51dd25c5fbf30434430d962d
cfittro <cdfittro@gmail.com>
2017-05-22 17:00:06 -0700

    Calculations set, not hovering

--- jquery.js
+++ jquery.js
@@ -8,27 +8,17 @@ $(document).ready(function () {
     $(".sub").hover(function() {
         $(this).css("backgroundColor", "black");
     }, function() {
-        $(this).css("backgroundColor", "white");
+         $(this).css("backgroundColor", "white");
     });
 
     $(".button").click(function () {
         $(".container").empty();
         var $boxes = prompt("Please input the number of boxes:");
+        var $wh = Math.sqrt($boxes);
+        var $whfloor = Math.floor($wh);
+        var $boxesfloor = $whfloor * $whfloor;
+        for (var i = 1; i <= $boxesfloor; i++) {
+            $(".container").append("<div class=sub></div>");}
+        $(".sub").css({'height': 800 / $whfloor,'width': 800 / $whfloor});
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
